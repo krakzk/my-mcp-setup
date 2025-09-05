60c97f24668e2f3801ce25427487251218b5ca39
Kashyap S P <kashyapsp@Kashyaps-Laptop.local>
2025-09-05 10:47:32 +0530

    Removed numeric

--- local_mcp_server.py
+++ local_mcp_server.py
@@ -7,19 +7,15 @@ def greet(name: str) -> str:
     """Greet user by name"""
     return f"Hello , {name}!"
 
-@mcp.tool
-def addNumbers(a: int, b: int) -> int:
-    """Adds two numbers"""
-    return a+b
 
 @mcp.tool(
-    name="Find character",
+    name="Find Actor",
     description="Search characters and find the one played by the actor name passed",
     tags={"actors", "characters"},
     meta={"version": "1.1", "author":"krakz"}
 )
 def find_character(character: str) -> list[dict]:
-    """Internal description, skipped as i specified above"""
+    """Search characters and find the one played by the actor name passed"""
     print(f"Searching for actor who played character '{character}'")
     return [{"id": 1, "character":"Iron Man", "actor": "Robert Downey Junior"}]
 
